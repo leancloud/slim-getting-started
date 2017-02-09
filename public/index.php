a182a543d1fd39da09a702d6b724682c5413ca92
Juvenn Woo <machese@gmail.com>
2017-02-09 23:12:23 +0800

    Fix url with dot route

--- public/index.php
+++ public/index.php
@@ -1,6 +1,15 @@
 <?php
 
 date_default_timezone_set('Asia/Shanghai');
+
+if (php_sapi_name() === 'cli-server') {
+    $url = parse_url($_SERVER["REQUEST_URI"]);
+    if ($url["path"] != "/" && file_exists(__DIR__ . $url["path"])) {
+        // Static file, serve it as-is
+        return false;
+    }
+}
+
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . "/../src/app.php";
 
