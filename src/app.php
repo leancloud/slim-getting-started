62e2d85aed5626092662e79dd9b5962c2ffcbe4a
Juvenn Woo <machese@gmail.com>
2017-10-30 11:52:03 +0800

    Remove useRegion

--- src/app.php
+++ src/app.php
@@ -30,7 +30,6 @@ Client::initialize(
 // 将 sessionToken 持久化到 cookie 中，以支持多实例共享会话
 Client::setStorage(new CookieStorage());
 Client::useProduction((getenv("LEANCLOUD_APP_ENV") === "production") ? true : false);
-Client::useRegion(getenv("LEANCLOUD_REGION"));
 
 SlimEngine::enableHttpsRedirect();
 $app->add(new SlimEngine());
